73efc0977c56787f0dcd40982430a23d24e7643f
Karthik.R <Karthick045@gmail.com>
2018-09-13 18:00:26 +0530

    practices

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,9 +3,20 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
-    styleUrls: []
+    styles: [`
+    .online{
+        color:white;
+    }
+`]
 })
 export class ServerComponent {
     serverId:number =70;
     serverStatus:String ='Online';
+constructor(){
+    this.serverStatus = Math.random()> 0.6 ? 'Online': 'Offline';
+}
+getColor(){
+    return this.serverStatus==='Online'?'green':'red';
+}
+
 }
